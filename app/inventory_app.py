3be70b919c4f604c8ff57f17f98520aeb53719d9
hiepnguyen034 <hiepnguyen034@gmail.com>
2019-04-10 17:54:13 -0400

    first test

--- app/inventory_app.py
+++ app/inventory_app.py
@@ -7,53 +7,58 @@ csv_headers = ["id", "name", "aisle", "department", "price"]
 #
 # READ
 #
+def read_from_csv(csv_filepath):
+	default_products = []
 
-products = []
+	with open(csv_filepath, "r") as csv_file:
+	    reader = csv.DictReader(csv_file)
+	    for ordered_dict in reader:
+	        default_products.append(dict(ordered_dict))
+	return default_products
 
-with open(csv_filepath, "r") as csv_file:
-    reader = csv.DictReader(csv_file)
-    for ordered_dict in reader:
-        products.append(dict(ordered_dict))
+if __name__ == "__main__":
+	csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+	products=read_from_csv(csv_filepath)
 
-#
-# LIST
-#
+	#
+	# LIST
+	#
 
-print("---------------------")
-print("LISTING PRODUCTS:")
+	print("---------------------")
+	print("LISTING PRODUCTS:")
 
-for product in products:
-    print(f"  {product['id']}) {product['name']} ")
+	for product in products:
+	    print(f"  {product['id']}) {product['name']} ")
 
-#
-# CREATE
-#
+	#
+	# CREATE
+	#
 
-print("---------------------")
-print("CREATING A NEW PRODUCT:")
+	print("---------------------")
+	print("CREATING A NEW PRODUCT:")
 
-new_product = {
-    "name": "My New Product",
-    "aisle": "Some Aisle",
-    "department": "Some Department",
-    "price": 99.99
-} # further exploration / todo: ask for user to input
+	new_product = {
+	    "name": "My New Product",
+	    "aisle": "Some Aisle",
+	    "department": "Some Department",
+	    "price": 99.99
+	} # further exploration / todo: ask for user to input
 
-next_id = len(products) + 1
+	next_id = len(products) + 1
 
-new_product["id"] = next_id
+	new_product["id"] = next_id
 
-print(new_product)
+	print(new_product)
 
-products.append(new_product)
+	products.append(new_product)
 
-#
-# WRITE
-#
+	#
+	# WRITE
+	#
 
-with open(csv_filepath, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader()
+	with open(csv_filepath, "w") as csv_file:
+	    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+	    writer.writeheader()
 
-    for product in products:
-        writer.writerow(product)
+	    for product in products:
+	        writer.writerow(product)
